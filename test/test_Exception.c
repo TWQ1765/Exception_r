18eb7c2bd514e77433a4da28196f43c19cee4c8a
TWQ1765 <www.taaadaaa@qq.com>
2018-06-28 10:22:57 +0800

    modify try catch statement.

--- test/test_Exception.c
+++ test/test_Exception.c
@@ -31,19 +31,41 @@ char *getName(int index){
 	return names[index];
 }
 
-void test_getName_Given_1_expect_David(void){
+void test_getName_Given_minus_1_expect_ERR_OUT_OF_BOUND(void){
 	CEXCEPTION_T e;
 	char *name;
 	
 	
 Try{
 	name = getName(-1);
-	printf("Name is %s\n", name);
+	TEST_FAIL_MESSAGE("Expected ERR_OUT_OF_BOUND to be thrown, but none.");
 }Catch(e) {
-	printf("error code: %d\n",e);
+	//printf("error code: %d\n",e);
 }
+}
+
+void test_getName_Given_4_expect_ERR_OUT_OF_BOUND(void){
+	CEXCEPTION_T e;
+	char *name;
 	
-	//name = getName(-1);
-	//TEST_ASSERT_EQUAL_STRING_LEN("David", name , 5);
-	//TEST_ASSERT_EQUAL_STRING("Davidx", name);
+	
+Try{
+	name = getName(4);
+	TEST_FAIL_MESSAGE("Expected ERR_OUT_OF_BOUND to be thrown, but none.");
+}Catch(e) {
+	//printf("error code: %d\n",e);
+}		
+}
+
+void test_getName_Given_1_expect_David(void){
+	CEXCEPTION_T e;
+	char *name;
+	
+	
+Try{
+	name = getName(1);
+	TEST_ASSERT_EQUAL_STRING("David", name);
+}Catch(e) {
+	TEST_FAIL_MESSAGE("Expected not to be thrown, but received one.");
+}		
 }
